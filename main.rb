831c0dda44f8291074006fe4ce485ecae3a8400e
Roberto Barros <roberto.f.barros@gmail.com>
2011-03-24 11:39:25 -0300

    criando um repositório GIT automaticamente

--- main.rb
+++ main.rb
@@ -3,3 +3,10 @@ BASE_URI = "https://github.com/RobertoBarros/new_app_template/raw/master/files"
 run "wget --no-check-certificate '#{BASE_URI}/GemFile' -O GemFile"
 
 run "bundle install"
+
+
+git :init
+git :add => '.'
+git :add => 'public/javascripts/rails.js --force'
+git :commit => '-am "Initial commit"'
+
