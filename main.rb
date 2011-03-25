2e1ea8e7ceb9496e1bf252d7223a1712537c8938
Roberto Barros <roberto.f.barros@gmail.com>
2011-03-25 00:25:07 -0300

    suporte a haml

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 BASE_URI = "https://github.com/RobertoBarros/new_app_template/raw/master/files"
 
 run "wget --no-check-certificate '#{BASE_URI}/GemFile' -O GemFile"
+run "wget --no-check-certificate '#{BASE_URI}/application.html.haml' -O app/views/layouts/application.html.haml"
 
 run "bundle install"
 
