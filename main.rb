f140e0cebdde8afc24422d49d11138c169f9fcda
Roberto Barros <roberto.f.barros@gmail.com>
2011-03-25 13:10:36 -0300

    adicionado cofiguração do asset_packages

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ BASE_URI = "https://github.com/RobertoBarros/new_app_template/raw/master/files"
 run "rm -Rf .gitignore public/index.html public/javascripts/* test app/views/layouts/*"
 
 
+run "wget --no-check-certificate '#{BASE_URI}/GemFile' -O GemFile"
 run "bundle install"
 
 plugin 'asset_packager', :git => 'git://github.com/sbecker/asset_packager.git'
@@ -21,11 +22,11 @@ run "compass init --using blueprint --app rails --css-dir public/stylesheets --s
 run "rm public/stylesheets/*"
 
 run "wget --no-check-certificate 'https://github.com/rails/jquery-ujs/raw/master/src/rails.js' -O public/javascripts/rails.js"
-run "wget --no-check-certificate '#{BASE_URI}/GemFile' -O GemFile"
 run "wget --no-check-certificate '#{BASE_URI}/application.html.haml' -O app/views/layouts/application.html.haml"
 run "wget --no-check-certificate '#{BASE_URI}/gitignore' -O .gitignore"
 run "wget --no-check-certificate '#{BASE_URI}/screen.scss' -O app/stylesheets/screen.scss"
 run "wget --no-check-certificate '#{BASE_URI}/print.scss' -O app/stylesheets/print.scss"
+run "wget --no-check-certificate '#{BASE_URI}/asset_packages.yml' -O config/asset_packages.yml"
 
 append_file 'config/environment.rb', <<-ASSET_PACKAGER
 
